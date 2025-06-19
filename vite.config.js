b0867f29fe310de197ce64b11e01aa2a9adb2459
Ibrahim Butt <ibrahim@example.com>
2025-06-19 09:14:00 +0500

    initialize

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
 // import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 
-// https://vite.dev/config/
+ 
 export default defineConfig({
    
     plugins: [react()],
-  // base: '/UI/',
->>>>>>> 61b773b (initialize)
+  base: '/UI',
 })
