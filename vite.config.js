8d72c61808dac5f7da4e71ff0fdeb926705a5cc0
Ibrahim Butt <ibrahim@example.com>
2025-06-19 13:20:26 +0500

    config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 
  
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/UI/' : '/',
+  base:  '/UI/',
 }))
